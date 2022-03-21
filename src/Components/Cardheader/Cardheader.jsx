8fd04779b4a341dce0fb7f949e2c823e3885d7bb
Pooran8898 <poorancoc@gmail.com>
2022-03-21 11:59:09 +0530

    Mock data added

--- src/Components/Cardheader/Cardheader.jsx
+++ src/Components/Cardheader/Cardheader.jsx
@@ -2,15 +2,28 @@ import React from "react";
 import "./Cardheader.css";
 import { BsFillGridFill } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 export const Cardheader = () => {
 
     return (
         <div className="cardheaderContainer">
             <div className="pagesContainer">
-                <h3 className="current">Your</h3>
-                <h3>All</h3>
-                <h3>Blocked</h3>
+                <h3 className="current">
+                    <Link to={"/"}>
+                        Your
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={"/all"}>
+                        All
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={"/blocked"}>
+                        Blocked
+                    </Link>
+                </h3>
             </div>
             <div className="demoContainer">
                 <BsFillGridFill />
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { DatacontextProvider } from "./Components/Datacontext/Datacontext";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <App />   
+      <DatacontextProvider>
+        <App />
+      </DatacontextProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
