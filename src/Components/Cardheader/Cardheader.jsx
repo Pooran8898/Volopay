ae82adc213b8c55e5b9db97db2a49d9ed7f74454
Pooran8898 <poorancoc@gmail.com>
2022-03-21 12:25:59 +0530

    Mock data updated with functionality to setdata based o n pathname

--- src/Components/Cardheader/Cardheader.jsx
+++ src/Components/Cardheader/Cardheader.jsx
@@ -1,25 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Cardheader.css";
 import { BsFillGridFill } from "react-icons/bs";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
+import { Datacontext } from "../Datacontext/Datacontext";
 
 export const Cardheader = () => {
-
+    const { pathname } = useContext(Datacontext);
     return (
         <div className="cardheaderContainer">
             <div className="pagesContainer">
-                <h3 className="current">
+                <h3 className={`${pathname === "/" ? "current" : ""}`}>
                     <Link to={"/"}>
                         Your
                     </Link>
                 </h3>
-                <h3>
+                <h3 className={`${pathname === "/all" ? "current" : ""}`}>
                     <Link to={"/all"}>
                         All
                     </Link>
                 </h3>
-                <h3>
+                <h3 className={`${pathname === "/blocked" ? "current" : ""}`}>
                     <Link to={"/blocked"}>
                         Blocked
                     </Link>
--- src/Components/Datacontext/Datacontext.jsx
+++ src/Components/Datacontext/Datacontext.jsx
@@ -1,13 +1,33 @@
-import React, { createContext } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { data } from "../../Utils/Data";
 
 
 export const Datacontext = createContext();
 
 
 export const DatacontextProvider = ({ children }) => {
+    const [ownerid, setownerid] = useState(1);
+    const { pathname } = useLocation();
+    const [pagedata, setpagedata] = useState([]);
 
+    useEffect(() => {
+        if (pathname === "/") {
+            let tempdata = data.filter((el) => el.owner_id === ownerid);
+            setpagedata(tempdata);
+        }
+        else if (pathname === "/all") {
+            setpagedata(data);
+        }
+        else if (pathname === "/blocked") {
+            let tempdata = data.filter((el) => el.status === "blocked");
+            setpagedata(tempdata);
+        }
+    }, [pathname, ownerid]);
+
+    
     return (
-        <Datacontext.Provider value={""}>
+        <Datacontext.Provider value={{ pathname, pagedata, setpagedata, setownerid }}>
             {children}
         </Datacontext.Provider>
     )
